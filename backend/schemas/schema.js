3e871774cd3efb8bfa30d0477529a2cf682be2cb
Abdul Samad <samadbinabdulla123@gmail.com>
2022-08-16 23:37:30 -0400

    contact backend

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -6,10 +6,11 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import about from './about'
 import skills from './skills'
 import works from './works'
+import contact from './contact'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    about,skills,works
+    about,skills,works,contact
   ]),
 })
